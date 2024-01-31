0326ccb44ced49e03178c5fb42aa9f503dc514b8
sakito <sakito@sakito.com>
2024-01-31 18:04:35 +0900

    node.jsのPATH修正

--- zsh.d/sync/node.zsh
+++
@@ -1,6 +0,0 @@
-#!/bin/env zsh
-
-# node
-export PATH=${PATH}:${HOME}/local/node/bin
-
-# end
---
+++ zsh.d/sync/nodejs.zsh
@@ -0,0 +1,7 @@
+#!/bin/env zsh
+
+# node.js 設定
+
+export PATH=${HOME}/.nodebrew/current/bin:${PATH}
+
+# end
