6c5abc0686c6dee45ff4d1717bc69f2ff23b2953
sakito <sakito@sakito.com>
2024-02-13 21:37:21 +0900

    nodeの設定を修正

--- zsh.d/sync/nodejs.zsh
+++ zsh.d/sync/nodejs.zsh
@@ -1,15 +1,12 @@
 #!/bin/env zsh
 
-# muse の設定
-eval "$(mise activate --shims zsh)"
+export PATH="$PATH:$HOME/opt/node/bin"
 
-# npm設定
-#NPM_CONFIG_PREFIX="${HOME}/.npm-packages"
-#export PATH="$PATH:$NPM_CONFIG_PREFIX/bin"
-#export MANPATH="${MANPATH-$(manpath)}:$NPM_CONFIG_PREFIX/share/man"
+NPM_PACKAGES="${HOME}/.npm-packages"
+export PATH="$PATH:$NPM_PACKAGES/bin"
+export MANPATH="${MANPATH-$(manpath)}:$NPM_PACKAGES/share/man"
 
-# nodeの設定
-#NODE_PATH=`npm root -g`
-#export NODE_PATH
+NODE_PATH=`npm root -g`
+export NODE_PATH
 
 # end
