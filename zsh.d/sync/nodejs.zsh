e633e29dec7e2bf08c4d14c2c947cc164d3a12c1
sakito <sakito@sakito.com>
2025-06-24 23:05:21 +0900

    nodeの設定コメントアウト

--- zsh.d/sync/nodejs.zsh
+++ zsh.d/sync/nodejs.zsh
@@ -1,10 +1,10 @@
 #!/bin/env zsh
 
-export PATH="$PATH:$HOME/opt/node/bin"
+#export PATH="$PATH:$HOME/opt/node/bin"
 
-NPM_PACKAGES="${HOME}/opt/node/lib/node_modules"
-export PATH="$PATH:$NPM_PACKAGES/bin"
-export MANPATH="${MANPATH-$(manpath)}:$NPM_PACKAGES/share/man"
+#NPM_PACKAGES="${HOME}/opt/node/lib/node_modules"
+#export PATH="$PATH:$NPM_PACKAGES/bin"
+#export MANPATH="${MANPATH-$(manpath)}:$NPM_PACKAGES/share/man"
 
 #NODE_PATH=`npm root -g`
 #export NODE_PATH
