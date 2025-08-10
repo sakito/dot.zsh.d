bb90a43acf794846595b04911d208edc8022ab5a
sakito <sakito@sakito.com>
2025-08-10 16:55:54 +0900

    完全にuvに移行したため、不要な設定をコメントアウト

--- zsh.d/sync/python.zsh
+++ zsh.d/sync/python.zsh
@@ -12,21 +12,21 @@ export VIRTUAL_ENV_DISABLE_PROMPT=true
 
 # uv
 source $HOME/.local/bin/env
-export PYENV_ROOT=${HOME}/opt/py
-source ${PYENV_ROOT}/${py3_version}/bin/activate
-export DYLD_FALLBACK_LIBRARY_PATH=~/.local/share/uv/python/cpython-${version}-macos-aarch64-none/lib
+#export PYENV_ROOT=${HOME}/opt/py
+#source ${PYENV_ROOT}/${py3_version}/bin/activate
+#export DYLD_FALLBACK_LIBRARY_PATH=~/.local/share/uv/python/cpython-${version}-macos-aarch64-none/lib
 
 #export PYTHONPATH=${HOME}/local/lib/python2.7/site-packages
 #export WORKON_HOME=${HOME}/.virtualenvs
 #source ${HOME}/local/bin/virtualenvwrapper.sh
-export PIP_RESPECT_VIRTUALENV=true
-if [ -d ${HOME}/python/OpenCV-2.4.2/build/lib ]; then
-    export PYTHONPATH=$PYTHONPATH:${HOME}/local/OpenCV-2.4.2/lib
-fi
+#export PIP_RESPECT_VIRTUALENV=true
+#if [ -d ${HOME}/python/OpenCV-2.4.2/build/lib ]; then
+#    export PYTHONPATH=$PYTHONPATH:${HOME}/local/OpenCV-2.4.2/lib
+#fi
 
 # PATH
 #export PATH=${HOME}/opt/py/py2.7/bin:${PATH}
-export PATH=${HOME}/opt/py/${py3_version}/bin:${PATH}
+#export PATH=${HOME}/opt/py/${py3_version}/bin:${PATH}
 
 # 関数
 function pjson {
